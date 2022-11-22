06b55ea9f29a6ca43d9f0c330fdbe4a7c2387899
Juan Arango <juanfer_arango@hotmail.com>
2022-11-22 01:55:51 -0500

    Search function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import markdown2
+from django import forms
 
 from . import util
 
@@ -25,3 +26,21 @@ def entry(request, title):
         "title": title
     })
 
+def search(request):
+    if request.method == "POST":
+        search = request.POST['q']
+        entry = convertMd(search)
+        if entry is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "info": entry,
+                "title": search
+            })
+        else:
+            listOfEntries = util.list_entries()
+            listOfSimilars = []
+            for data in listOfEntries:
+                if search.upper() in data.upper():
+                    listOfSimilars.append(data)
+            return render(request, "encyclopedia/search.html", {
+                "similars":listOfSimilars
+            })
